d28751ea92d7326e2a4841f33ea57bb21d0004a9
Krisztian Sala <salakrisztian@gmail.com>
2022-08-07 22:51:55 +0300

    Log response status code

--- main.go
+++ main.go
@@ -47,10 +47,24 @@ func init() {
 	log.SetLevel(log.InfoLevel)
 }
 
-func logRequest(handler http.Handler) http.Handler {
+type StatusRecorder struct {
+	http.ResponseWriter
+	Status int
+}
+
+func (r *StatusRecorder) WriteHeader(status int) {
+	r.Status = status
+	r.ResponseWriter.WriteHeader(status)
+}
+
+func WithLogging(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
-		handler.ServeHTTP(w, r)
+		recorder := &StatusRecorder{
+			ResponseWriter: w,
+			Status:         200,
+		}
+		handler.ServeHTTP(recorder, r)
+		log.Printf("%s %s %d %s", r.RemoteAddr, r.Method, recorder.Status, r.URL)
 	})
 }
 
@@ -92,7 +106,7 @@ func main() {
 
 	srv := &http.Server{
 		Addr:    fmt.Sprintf("%s:%d", listenAddress, port),
-		Handler: logRequest(http.DefaultServeMux),
+		Handler: WithLogging(http.DefaultServeMux),
 	}
 
 	go func() {
